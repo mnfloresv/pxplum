bc347a6a3c91d52b644d1030ac21ae7d3f97f9fa
Manuel Flores <manuelfloresv@gmail.com>
2011-04-04 23:59:35 +0200

    Add dhcp server and tftp server for pxe boot

---
+++ pxplum-server/rootfs/etc/init.d/local.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# /etc/init.d/local.sh - Local startup commands.
+#
+# All commands here will be executed at boot time.
+#
+. /etc/init.d/rc.functions
+
+echo "Starting local startup commands... "
+
+ifconfig eth0 10.0.0.1
+
